bcdafc443992e7716320444c31cb9ba05778353f
jojo2357 <>
2021-02-01 13:28:15 -0800

    Reinstate last nite dev and redoc

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,55 +1,83 @@
 package frc.robot;
 
+import frc.misc.InitializationFailureException;
+import frc.robot.robotconfigs.AbstractConfig;
+
 public class RobotMap {
-    //public static final int [NAME] = [ID];
-    //public static final int wristMotor = 5; <-- EXAMPLE
-    public static final String GOAL_CAM_NAME = "GoalCamera";
-    public static final String BALL_CAM_NAME = "BallCamera";
+    public static final AbstractConfig getNumbersFrom = RobotToggles.getNumbersFrom;
+
+    public static final String GOAL_CAM_NAME;
+    public static final String BALL_CAM_NAME;
 
     //Drive Motors
-    public static final int DRIVE_LEADER_L = 1; //talon
-    public static final int[] DRIVE_FOLLOWERS_L = {2, 3}; //talon
-
-    public static final int DRIVE_LEADER_R = 4; //talon
-    public static final int[] DRIVE_FOLLOWERS_R = {5, 6}; //talon
+    public static final int DRIVE_LEADER_L;
+    public static final int[] DRIVE_FOLLOWERS_L;
+    
+    public static final int DRIVE_LEADER_R;
+    public static final int[] DRIVE_FOLLOWERS_R;
 
     //Shooter Motors
-    public static final int SHOOTER_LEADER = 7; //talon
-    public static final int SHOOTER_FOLLOWER = 8; //talon
-
-    /*
-    //drive motors
-    public static final int driveLeaderL = 1; //neo
-    public static final int driveFollowerL1 = 2; //neo
-    public static final int driveFollowerL2 = 3; //neo
+    public static final int SHOOTER_LEADER;
+    public static final int SHOOTER_FOLLOWER;
     
-    public static final int driveLeaderR = 4; //neo
-    public static final int driveFollowerR1 = 5; //neo
-    public static final int driveFollowerR2 = 6; //neo
-    //shooter motors
-    public static final int shooterLeader = 7; //neo
-    public static final int shooterFollower = 8;  //neo
-    */
     //turret
-    public static final int TURRET_YAW = 33; //550
+    public static final int TURRET_YAW;
     //climber
-    public static final int CLIMBER_A = 8; //victor
-    public static final int CLIMBER_B = 9; //victor
+    public static final int CLIMBER_A;
+    public static final int CLIMBER_B;
     //hopper
-    public static final int AGITATOR_MOTOR = 10; //victor
-    public static final int INDEXER_MOTOR = 11; //victor
+    public static final int AGITATOR_MOTOR;
+    public static final int INDEXER_MOTOR;
     //intake
-    public static final int INTAKE_MOTOR = 12; //victor
+    public static final int INTAKE_MOTOR;
 
-    public static final int PIGEON = 22; //pigeon
-    public static final int PCM = 23; //pcm
+    public static final int PIGEON;
+    public static final int PCM;
 
     //pneumatics
-    public static final int INTAKE_OUT = 4;
-    public static final int INTAKE_IN = 5;
-    public static final int BUDDY_UNLOCK = 0;
-    public static final int SHIFTERS = 6;
-    public static final int CLIMBER_LOCK_IN = 2;
-    public static final int CLIMBER_LOCK_OUT = 3;
+    public static final int INTAKE_OUT;
+    public static final int INTAKE_IN;
+    public static final int BUDDY_UNLOCK;
+    public static final int SHIFTERS;
+    public static final int CLIMBER_LOCK_IN;
+    public static final int CLIMBER_LOCK_OUT;
+
+    //@author jojo2357
+    static {
+        try {
+            GOAL_CAM_NAME = (String) getNumbersFrom.getClass().getField("GOAL_CAM_NAME").get(getNumbersFrom);
+            BALL_CAM_NAME = (String) getNumbersFrom.getClass().getField("BALL_CAM_NAME").get(getNumbersFrom);
+
+            //Drive Motors
+            DRIVE_LEADER_L = getNumbersFrom.getClass().getField("DRIVE_LEADER_L").getInt(getNumbersFrom);
+            DRIVE_FOLLOWERS_L = (int[]) getNumbersFrom.getClass().getField("DRIVE_FOLLOWERS_L").get(getNumbersFrom);
+
+            DRIVE_LEADER_R = getNumbersFrom.getClass().getField("DRIVE_LEADER_L").getInt(getNumbersFrom);
+            DRIVE_FOLLOWERS_R = (int[]) getNumbersFrom.getClass().getField("DRIVE_FOLLOWERS_R").get(getNumbersFrom);
+
+            //Shooter Motors
+            SHOOTER_LEADER = getNumbersFrom.getClass().getField("SHOOTER_LEADER").getInt(getNumbersFrom);
+            SHOOTER_FOLLOWER = getNumbersFrom.getClass().getField("SHOOTER_FOLLOWER").getInt(getNumbersFrom);
+
+            TURRET_YAW = getNumbersFrom.getClass().getField("TURRET_YAW").getInt(getNumbersFrom);
+            CLIMBER_A = getNumbersFrom.getClass().getField("CLIMBER_A").getInt(getNumbersFrom);
+            CLIMBER_B = getNumbersFrom.getClass().getField("CLIMBER_B").getInt(getNumbersFrom);
+            AGITATOR_MOTOR = getNumbersFrom.getClass().getField("AGITATOR_MOTOR").getInt(getNumbersFrom);
+            INDEXER_MOTOR = getNumbersFrom.getClass().getField("INDEXER_MOTOR").getInt(getNumbersFrom);
+            INTAKE_MOTOR = getNumbersFrom.getClass().getField("INTAKE_MOTOR").getInt(getNumbersFrom);
+
+            PIGEON = getNumbersFrom.getClass().getField("PIGEON").getInt(getNumbersFrom);
+            PCM = getNumbersFrom.getClass().getField("PCM").getInt(getNumbersFrom);
+
+            INTAKE_OUT = getNumbersFrom.getClass().getField("INTAKE_OUT").getInt(getNumbersFrom);
+            INTAKE_IN = getNumbersFrom.getClass().getField("INTAKE_IN").getInt(getNumbersFrom);
+            BUDDY_UNLOCK = getNumbersFrom.getClass().getField("BUDDY_UNLOCK").getInt(getNumbersFrom);
+            SHIFTERS = getNumbersFrom.getClass().getField("SHIFTERS").getInt(getNumbersFrom);
+            CLIMBER_LOCK_IN = getNumbersFrom.getClass().getField("CLIMBER_LOCK_IN").getInt(getNumbersFrom);
+            CLIMBER_LOCK_OUT = getNumbersFrom.getClass().getField("CLIMBER_LOCK_OUT").getInt(getNumbersFrom);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new InitializationFailureException(e.toString(), "");
+        }
+    }
 
 }
--- src/main/java/frc/robot/RobotToggles.java
+++ src/main/java/frc/robot/RobotToggles.java
@@ -1,39 +1,78 @@
 package frc.robot;
 
 import frc.drive.DriveTypes;
+import frc.misc.InitializationFailureException;
+import frc.robot.robotconfigs.AbstractConfig;
+import frc.robot.robotconfigs.Robot2020;
 
 public class RobotToggles {
-    public static final boolean DEBUG = true;
+    /**
+     * If you change this ONE SINGULAR VARIBLE the ENTIRE CONFIG WILL CHANGE.
+     * Use this to select which robot you are using from the list under robotconfigs
+     */
+    public static final AbstractConfig getNumbersFrom = new Robot2020();
+
+    public static final boolean DEBUG;
     //Subsystems
 
     /**
      * Enables the drive
      */
-    public static final boolean ENABLE_DRIVE = true;
-    public static final boolean ENABLE_INTAKE = false;
-    public static final boolean ENABLE_CLIMBER = false;
-    public static final boolean ENABLE_SHOOTER = false;
-    public static final boolean ENABLE_HOPPER = false;
-
+    public static final boolean ENABLE_DRIVE;
+    public static final boolean ENABLE_INTAKE;
+    public static final boolean ENABLE_CLIMBER;
+    public static final boolean ENABLE_SHOOTER;
+    public static final boolean ENABLE_HOPPER;
 
     //Drivetrain
-    public static final boolean DRIVE_USE_SPARKS = true;
-    public static final boolean DRIVE_USE_6_MOTORS = true;
-    public static final boolean DRIVE_INVERT_LEFT = true;
-    public static final boolean DRIVE_INVERT_RIGHT = false;
-    public static final DriveTypes EXPERIMENTAL_DRIVE = DriveTypes.STANDARD;
-    public static final boolean CALIBRATE_DRIVE_PID = false;
+    public static final boolean DRIVE_USE_SPARKS;
+    public static final boolean DRIVE_USE_6_MOTORS;
+    public static final boolean DRIVE_INVERT_LEFT;
+    public static final boolean DRIVE_INVERT_RIGHT;
+    public static final DriveTypes EXPERIMENTAL_DRIVE;
+    public static final boolean CALIBRATE_DRIVE_PID;
 
     //Misc
-    public static final boolean ENABLE_VISION = false;
-    public static final boolean USE_PHOTONVISION = true;
-    public static final boolean ENABLE_IMU = false;
-    public static final boolean USE_PIGEON = false;
-    public static final boolean USE_NAVX2 = false;
-
+    public static final boolean ENABLE_VISION;
+    public static final boolean USE_PHOTONVISION;
+    public static final boolean ENABLE_IMU;
+    public static final boolean USE_PIGEON;
+    public static final boolean USE_NAVX2;
 
     //SHOOTER
-    public static final boolean SHOOTER_USE_SPARKS = false;
-    public static final boolean SHOOTER_USE_TWO_MOTORS = true;
-    public static final boolean SHOOTER_INVERTED = true;
+    public static final boolean SHOOTER_USE_SPARKS;
+    public static final boolean SHOOTER_USE_TWO_MOTORS;
+    public static final boolean SHOOTER_INVERTED;
+
+    //@author jojo2357
+    static {
+        try {
+            DEBUG = getNumbersFrom.getClass().getField("DEBUG").getBoolean(getNumbersFrom);
+
+            ENABLE_DRIVE = getNumbersFrom.getClass().getField("ENABLE_DRIVE").getBoolean(getNumbersFrom);
+            ENABLE_INTAKE = getNumbersFrom.getClass().getField("ENABLE_INTAKE").getBoolean(getNumbersFrom);
+            ENABLE_CLIMBER = getNumbersFrom.getClass().getField("ENABLE_CLIMBER").getBoolean(getNumbersFrom);
+            ENABLE_SHOOTER = getNumbersFrom.getClass().getField("ENABLE_SHOOTER").getBoolean(getNumbersFrom);
+            ENABLE_HOPPER = getNumbersFrom.getClass().getField("ENABLE_HOPPER").getBoolean(getNumbersFrom);
+
+            DRIVE_USE_SPARKS = getNumbersFrom.getClass().getField("DRIVE_USE_SPARKS").getBoolean(getNumbersFrom);
+            DRIVE_USE_6_MOTORS = getNumbersFrom.getClass().getField("DRIVE_USE_6_MOTORS").getBoolean(getNumbersFrom);
+            DRIVE_INVERT_LEFT = getNumbersFrom.getClass().getField("DRIVE_INVERT_LEFT").getBoolean(getNumbersFrom);
+            DRIVE_INVERT_RIGHT = getNumbersFrom.getClass().getField("DRIVE_INVERT_RIGHT").getBoolean(getNumbersFrom);
+            EXPERIMENTAL_DRIVE = (DriveTypes) getNumbersFrom.getClass().getField("DRIVE_INVERT_RIGHT").get(getNumbersFrom);
+            CALIBRATE_DRIVE_PID = getNumbersFrom.getClass().getField("CALIBRATE_DRIVE_PID").getBoolean(getNumbersFrom);
+
+            ENABLE_VISION = getNumbersFrom.getClass().getField("ENABLE_VISION").getBoolean(getNumbersFrom);
+            USE_PHOTONVISION = getNumbersFrom.getClass().getField("USE_PHOTONVISION").getBoolean(getNumbersFrom);
+            ENABLE_IMU = getNumbersFrom.getClass().getField("ENABLE_IMU").getBoolean(getNumbersFrom);
+            USE_PIGEON = getNumbersFrom.getClass().getField("USE_PIGEON").getBoolean(getNumbersFrom);
+            USE_NAVX2 = getNumbersFrom.getClass().getField("USE_NAVX2").getBoolean(getNumbersFrom);
+
+            SHOOTER_USE_SPARKS = getNumbersFrom.getClass().getField("SHOOTER_USE_SPARKS").getBoolean(getNumbersFrom);
+            SHOOTER_USE_TWO_MOTORS = getNumbersFrom.getClass().getField("SHOOTER_USE_TWO_MOTORS").getBoolean(getNumbersFrom);
+            SHOOTER_INVERTED = getNumbersFrom.getClass().getField("SHOOTER_INVERTED").getBoolean(getNumbersFrom);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new InitializationFailureException(e.toString(), "");
+        }
+    }
 }
