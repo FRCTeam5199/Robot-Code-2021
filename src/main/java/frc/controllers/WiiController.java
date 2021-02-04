367d8eaab1c01277e10840b1baa3f46d6dba0ad7
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-02-03 17:33:12 -0800

    Blake doc (#13)
    * WPILib update. (#9)
    
    * Slalom Path
    
    * JavaDoc
    
    ButtonPanel, WiiController
    
    Co-authored-by: Smaltin <sterling.maltin@gmail.com>
    Co-authored-by: jojo2357 <>
    Co-authored-by: blakemiele <77865936+blakemiele@users.noreply.github.com>

--- src/main/java/frc/controllers/WiiController.java
+++ src/main/java/frc/controllers/WiiController.java
@@ -1,14 +1,21 @@
 package frc.controllers;
 
 public class WiiController extends BaseController{
+    
     public WiiController(int n){
         super(n);
     }
 
+    /**
+     * Gets the Raw axis value starting at 0
+     */
     public double get(ControllerEnums.WiiAxis axis){
         return stick.getRawAxis(axis.AXIS_VALUE);
     }
 
+    /**
+     * Gets the Raw button value and returns true if it is pressed when it is run
+     */
     public ControllerEnums.ButtonStatus get(ControllerEnums.WiiButton button){
         return ControllerEnums.ButtonStatus.get(stick.getRawButton(button.AXIS_VALUE));
     }
