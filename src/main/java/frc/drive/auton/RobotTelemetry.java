984faf3893cfb540397f41751131a790795318ab
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-02-03 17:23:50 -0800

    Javadocs (#12)
    Intake, RobotTelemetry, DriveManager
    
    Co-authored-by: Andrea Gajic <andreagajic101@gmail.com>

--- src/main/java/frc/drive/auton/RobotTelemetry.java
+++ src/main/java/frc/drive/auton/RobotTelemetry.java
@@ -30,6 +30,10 @@ public class RobotTelemetry {
     }
 
     //pls yes
+    /**
+     * @param x 
+     * @param y 
+     */
     public double headingErrorWraparound(double x, double y) {
         return UtilFunctions.mathematicalMod(headingError(x, y) + 180, 360) - 180;
     }
@@ -66,6 +70,10 @@ public class RobotTelemetry {
         return UtilFunctions.mathematicalMod(yawRel() + 180, 360) - 180;
     }
 
+
+    /**
+     * @return relative to start yaw of pigeon
+     */
     public double yawRel() { //return relative(to start) yaw of pigeon
         updatePigeon();
         return (ypr[0] - startYaw);
@@ -87,79 +95,129 @@ public class RobotTelemetry {
         startYaw = yawAbs();
     }
 
-    public double yawAbs() { // return absolute yaw of pigeon
+    /**
+     * @return absolute yaw of pigeon
+     */
+    public double yawAbs() {  //get absolute yaw of pigeon
         updatePigeon();
         return ypr[0];
     }
 
-    //getFPS - get wheel FPS from encoder
-    public double getFPSLeft() {
+    /**
+     * @return wheel FPS from encoder
+     */
+    public double getFPSLeft() { //get wheel FPS from encoder
         return getIPSLeft() / 12;
     }
 
     //getIPS - get wheel IPS from encoder
+    /**
+     * @return wheel IPS from encoder
+     */
     public double getIPSLeft() {
         return (getRPMLeft() * wheelCircumference()) / 60;
     }
 
     //getRPM - get wheel RPM from encoder
+    /** 
+     * @return wheel RPM from encoder for lL
+     */
     public double getRPMLeft() {
         return (driver.leaderL.getEncoder().getVelocity()) / 9;
     }
 
     //position conversion -------------------------------------------------------------------------------------------------------
+    /**
+     * @return circumference of wheel
+     */
     private double wheelCircumference() {
         return RobotNumbers.WHEEL_DIAMETER * Math.PI;
     }
 
+    /**
+     * @return FPS from encoder
+     */
     public double getFPSRight() {
         return getIPSRight() / 12;
     }
 
+    /**
+     * @return IPS from encoder
+     */
     public double getIPSRight() {
         return (getRPMRight() * wheelCircumference()) / 60;
     }
 
+    /**
+     * @return wheel RPM from encoder for lR
+     */
     public double getRPMRight() {
         return (driver.leaderR.getEncoder().getVelocity()) / 9;
     }
 
     //getInches - get wheel inches traveled
+    /**
+     * @return wheel inches traveled
+     */
     public double getInchesLeft() {
         return (getRotationsLeft() * wheelCircumference());
     }
 
     //getRotations - get wheel rotations on encoder
+    /**
+     * @return wheel rotations for lL
+     */
     public double getRotationsLeft() {
         return (driver.leaderL.getEncoder().getPosition()) / 9;
     }
 
+    /**
+     * @return wheel inches traveled
+     */
     public double getInchesRight() {
         return (getRotationsRight() * wheelCircumference());
     }
 
+    /**
+     * @return wheel rotations for lR
+     */
     public double getRotationsRight() {
         return (driver.leaderR.getEncoder().getPosition()) / 9;
     }
 
     //getMeters - get wheel meters traveled
+    /**
+     * @return wheel meters traveled
+     */
     public double getMetersLeft() {
         return Units.feetToMeters(getFeetLeft());
     }
 
     //getFeet - get wheel feet traveled
+    /**
+     * @return wheel feet traveled
+     */
     public double getFeetLeft() {
         return (getRotationsLeft() * wheelCircumference() / 12);
     }
 
+    /**
+     * @return wheel meters traveled
+     */
     public double getMetersRight() {
         return Units.feetToMeters(getFeetRight());
     }
 
+    /**
+     * @return wheel feet right
+     */
     public double getFeetRight() {
         return (getRotationsRight() * wheelCircumference() / 12);
     }
 
+    /**
+     * @return velocity from encoder for lL
+     */
     public double getLeftVelocity() {
         if (RobotToggles.DRIVE_USE_SPARKS) {
             return driver.leaderL.getEncoder().getVelocity();
@@ -169,6 +227,9 @@ public class RobotTelemetry {
         return 0;
     }
 
+    /**
+     * @return velocity from encoder for lR
+     */
     public double getRightVelocity() {
         if (RobotToggles.DRIVE_USE_SPARKS) {
             return driver.leaderR.getEncoder().getVelocity();
