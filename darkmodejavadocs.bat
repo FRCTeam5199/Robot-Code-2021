90cf3abe0eaa30c816d88ce42eccb75f8f5aabd0
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-02-22 11:28:07 -0800

    Launches javadocs on style change and removes temp file

--- darkmodejavadocs.bat
+++ darkmodejavadocs.bat
@@ -4,5 +4,11 @@ dir /s/b stylesheet.css>myfile.dat
 
 for /F "tokens=*" %%A in (myfile.dat) do if not "%%A"=="%cd%\darkmodestylesheet.css" copy /Y "darkmodestylesheet.css" "%%A" >NUL
 
+dir /s/b index.html>myfile.dat
+
+for /F "tokens=*" %%A in (myfile.dat) do "%%A" 
+
+del myfile.dat
+
 if ERRORLEVEL 1 (echo Fail. Your eyes must burn) else (echo Success. Your eyes are saved)
 pause>NUL
--- lightmodejavadocs.bat
+++ lightmodejavadocs.bat
@@ -4,5 +4,11 @@ dir /s/b stylesheet.css>myfile.dat
 
 for /F "tokens=*" %%A in (myfile.dat) do if not "%%A"=="%cd%\lightmodestylesheet.css" copy /Y "lightmodestylesheet.css" "%%A" >NUL
 
+dir /s/b index.html>myfile.dat
+
+for /F "tokens=*" %%A in (myfile.dat) do "%%A"
+
+del myfile.dat
+
 if ERRORLEVEL 1 (echo Fail. You get what you deserve) else (echo Success. Your are burning)
 pause>NUL
