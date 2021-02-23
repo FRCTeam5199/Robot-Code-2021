be07237b339b37517e07ec16f11ca1c75ce79a22
yoshidog2005 <brandotl@icloud.com>
2021-02-22 17:53:55 -0800

    plays random music on disable

--- src/main/java/frc/misc/Chirp.java
+++ src/main/java/frc/misc/Chirp.java
@@ -5,13 +5,25 @@ import edu.wpi.first.wpilibj.Filesystem;
 import frc.motors.TalonMotor;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Chirp extends Orchestra{
     public static final ArrayList<TalonMotor> talonMotorArrayList = new ArrayList<>();
+    public static final String[] musicNames = {"BokuNoSensou_4Motors", "CoconutMall_4Motors", "Electroman_Adventures_4Motors", "Imperial_March_4Motors", "kiraTheme_4Motors", "Megalovania_4Motors", "TheOnlyThingTheyFearlsYou_4Motors", "WiiSports_4Motors"};
+    Random musicRand = new Random();
+    int upperbound = 8;
+    int randomMusic = musicRand.nextInt(upperbound);
+
 
     public Chirp() {
         initChirp();
     }
+
+    public void disabledChirp(){
+        if(!isPlaying()){
+            loadMusic(Filesystem.getDeployDirectory().toPath().resolve("sounds/" + musicNames[randomMusic] + ".chrp").toString());
+        }
+    } 
 
     public void initChirp() {
         for (TalonMotor motor : talonMotorArrayList) {
@@ -19,7 +31,12 @@ public class Chirp extends Orchestra{
         }
     }
 
+    
     public void loadSound(String soundName) {
+        
         loadMusic(Filesystem.getDeployDirectory().toPath().resolve("sounds/" + soundName + ".chrp").toString());
     }
+    
+    
+    
 }
